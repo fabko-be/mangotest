f804fb348e8dbbd4c2f721ba43462c1df607e619
Fabian Quoidbach <fabianquoidbach@gmail.com>
2020-10-28 11:32:04 +0100

    Init repo

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Mango Download</title>
+      </Head>
+      <p>Hello World!</p>
+    </div>
+  )
+}
